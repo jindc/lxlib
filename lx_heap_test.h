e8d2682cd051337f629e0908b1baada58cce82e7
德才 <jindc@163.com>
2015-03-21 09:28:35 +0800

    Create lx_heap_test.h

---
+++ lx_heap_test.h
@@ -0,0 +1,48 @@
+#ifndef LX_HEAP_TEST_H
+#define LX_HEAP_TEST_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include "lx_max_heap.h"
+#include "lx_random.h"
+
+void print_arr(uint64_t * arr,int size)
+{
+    size_t i;
+
+    for(i = 0; i< size; ++i)
+        printf(" %lld",arr[i]);
+    printf("\n");
+}
+
+int lx_heap_test(int argc, char *argv[])
+//int main(int argc, char *argv[])
+{
+    int size = 100;
+    uint64_t *arr,maxnum,i;   
+    
+    if(argc > 1)
+        size = atoi(argv[1]);
+    arr =(uint64_t *) malloc(size * sizeof(uint64_t));
+    if(arr == NULL)
+        return EXIT_FAILURE;
+
+    lx_rand_uint64_arr(arr,size);
+    if(size < 50)
+        print_arr(arr,size);
+
+    lx_heap_sort(arr,size);
+
+    if(size < 50)
+        print_arr(arr,size);
+    
+    if(size > 1)
+        for( i = 1;i < size;i++)
+            assert(arr[i-1] <= arr[i]);
+    
+    free(arr);
+}
+
+#endif
